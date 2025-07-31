543a58acf0978266992d86166081bee68e793eb8
Yitzchak Cohen <yitzchakncohen@gmail.com>
2025-07-31 19:52:37 -0400

    Updated Clues

--- My project/Assets/Scripts/Clue.cs
+++ My project/Assets/Scripts/Clue.cs
@@ -1,40 +1,45 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Clue : MonoBehaviour
 {
     private const string GREY_LEVEL_PROPERTY = "greyLevel";
     public bool IsScanned => isScanned;
-    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private List<MeshRenderer> meshRenderers;
     [SerializeField] private GameObject scannedEffect;
-    private Material material;
+    private List<Material> materials;
     private float highlightAmount = 0.7f;
     private bool isScanned = false;
 
     private void Awake()
     {
-        material = meshRenderer.material;
-        material.SetFloat(GREY_LEVEL_PROPERTY, 1f);
+        materials = new List<Material>();
+        foreach (MeshRenderer meshRenderer in meshRenderers)
+        {
+            meshRenderer.material.SetFloat(GREY_LEVEL_PROPERTY, 1f);
+            materials.Add(meshRenderer.material);
+        }
     }
 
     public void Target()
     {
-        material.SetFloat(GREY_LEVEL_PROPERTY, highlightAmount);
+        foreach (Material material in materials) material.SetFloat(GREY_LEVEL_PROPERTY, highlightAmount);
     }
 
     public void Reset()
     {
-        material.SetFloat(GREY_LEVEL_PROPERTY, 1.0f);
+        foreach (Material material in materials) material.SetFloat(GREY_LEVEL_PROPERTY, 1.0f);
     }
 
     public void UpdateScan(float percentageComplete)
     {
-        material.SetFloat(GREY_LEVEL_PROPERTY, Mathf.Lerp(highlightAmount, 0f, percentageComplete));
+        foreach (Material material in materials) material.SetFloat(GREY_LEVEL_PROPERTY, Mathf.Lerp(highlightAmount, 0f, percentageComplete));
     }
 
     public void SetScanned()
     {
         isScanned = true;
-        material.SetFloat(GREY_LEVEL_PROPERTY, 0f);
+        foreach (Material material in materials) material.SetFloat(GREY_LEVEL_PROPERTY, 0f);
         scannedEffect.SetActive(true);
     }
 }
